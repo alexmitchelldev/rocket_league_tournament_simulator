bed0e7f3dfe17c465287b9566ddb1be92f64e104
Alex Mitchell <amitchell@ctatax.uk.com>
2022-10-11 21:24:06 +0100

    Add Player class and initSimulator function

---
+++ rl_tournament_simulator.js
@@ -0,0 +1,13 @@
+const initSimulator = () => {
+    console.log('Welcome!');
+};
+
+class Player {
+    constructor(name, attack, defence) {
+        this.name       = name;
+        this.attack     = attack;
+        this.defence    = defence;
+    }
+}
+
+initSimulator();
